e412e011277947076a45ad03de297a1dff38f6a3
Patrick Mellon <mellon.patrick.j@gmail.com>
2017-04-13 07:04:18 -0400

    Update index,style,and script files

---
+++ script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+  $("#1").click(function callback() {
+    $("#content").empty()
+
+    $("#content").append("I Make Great Pie")
+
+  })
+
+  $("#2").click(function() {
+    $("#content").empty()
+
+    $("#content").append("You'll find Yummy Pies.")
+
+  })
+
+  $("#3").click(function() {
+    $("#content").empty()
+
+    $("#content").append("Call Papa John's")
+
+  })
+
+});
